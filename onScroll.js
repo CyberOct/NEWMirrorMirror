67f6d966da119c340566ce560a3747bf75a581d5
Stefan Butura <stefan.butura@gmail.com>
2020-08-30 13:06:55 +0300

    update

--- onScroll.js
+++ onScroll.js
@@ -1,14 +1,24 @@
 const onScroll = () => {
     const aboutUsImg = document.getElementsByClassName('aboutUsImg');
-    const container = document.getElementsByClassName('aboutUsImgContainer');
-    const orangeWave = document.getElementsByClassName('orangeWave')
+    const container = document.getElementsByClassName('description');
+    const orangeWave = document.getElementsByClassName('orangeWave');
+    const ourMission = document.getElementsByClassName('ourMission');
+    const ourFeatures = document.getElementsByClassName('card');
     
     window.addEventListener('scroll', () => {
         console.log(window.pageYOffset);
-        if(window.pageYOffset >= 500){
-            container[0].style.animation = 'slideDown 1s ease forwards 1s';
-            aboutUsImg[0].style.animation = 'slideIn 2s ease forwards 1s';
-            orangeWave[0].style.animation = 
+        console.log(ourFeatures);
+        if(window.pageYOffset >= 600){
+            container[0].style.animation = 'slideDown 1.5s cubic-bezier(0.4, 0, 1, 1) forwards 1s';
+            aboutUsImg[0].style.animation = 'slideIn 1.5s cubic-bezier(0.4, 0, 1, 1) forwards 1s';
+           
+        }
+        if (window.pageYOffset >= 1400) {
+            orangeWave[0].style.animation = 'f 4s cubic-bezier(0.4, 0, 1, 1) forwards 1s';
+            ourMission[0].style.animation = 'slideDown 0.5s cubic-bezier(0.4, 0, 1, 1) forwards 2s';
+            for (i = 0; i < ourFeatures.length; i++) {
+                ourFeatures[i].style.animation = `navLinkFade 0.5s ease forwards ${i / 7 + 0.3}s`;
+            };
         }
     })
 }
